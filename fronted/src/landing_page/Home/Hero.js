6a8c579ecee43f0cb429785b72be6c7e866f1c1d
Mahmadayan hakeem <ayanhakeem20@gmail.com>
2026-04-26 18:30:14 +0530

    Fixed signup button redirection on landing page

--- fronted/src/landing_page/Home/Hero.js
+++ fronted/src/landing_page/Home/Hero.js
@@ -1,17 +1,29 @@
-import React from 'react'
+import { Link } from "react-router-dom";
 
-
- function Hero() {
-    return (  
-        <div className='container p-5 mb-5'>
-            <div className='row text-center'>
-                <img src='media/images/homeHero.png' alt='Hero Image' className='mb-5'/>
-                <h1 className='mt-5 mb-3'>Invest in everything</h1>
-                <p className='mb-5'>Online platform to invest in stocks, derivatives, mutual funds, and more</p>
-                <button className='p-2 btn btn-primary fs-5 mb-5' style={{width:"20%",margin:"0 auto"}}>Signup Now</button>
-            </div>
-        </div>
-    );
- }
+function Hero() {
+  return (
+    <div className="container p-5 mb-5">
+      <div className="row text-center">
+        <img
+          src="media/images/homeHero.png"
+          alt="Hero Image"
+          className="mb-5"
+        />
+        <h1 className="mt-5 mb-3">Invest in everything</h1>
+        <p className="mb-5">
+          Online platform to invest in stocks, derivatives, mutual funds, and
+          more
+        </p>
+        <Link
+          to="/signup"
+          className="p-2 btn btn-primary fs-5 mb-5"
+          style={{ width: "20%", margin: "0 auto" }}
+        >
+          Signup Now
+        </Link>
+      </div>
+    </div>
+  );
+}
  
  export default Hero
--- fronted/src/landing_page/Products/Universe.js
+++ fronted/src/landing_page/Products/Universe.js
@@ -1,4 +1,4 @@
-import React from "react";
+import { Link } from "react-router-dom";
 
 function Universe() {
   return (
@@ -11,35 +11,61 @@ function Universe() {
         </p>
 
         <div className="col-4 p-3 mt-5">
-          <img src="media/images/smallcaseLogo.png" />
+          <img src="media/images/smallcaseLogo.png" alt="smallcase" />
           <p className="text-small text-muted">Thematic investment platform</p>
         </div>
         <div className="col-4 p-3 mt-5">
-          <img className="mb-3" src="media/images/streakLogo.png" style={{width:"200px"}}/>
+          <img
+            className="mb-3"
+            src="media/images/streakLogo.png"
+            style={{ width: "200px" }}
+            alt="streak"
+          />
           <p className="text-small text-muted">Algo & strategy platform</p>
         </div>
         <div className="col-4 p-3 mt-5">
-          <img className="mb-3" src="media/images/sensibullLogo.svg" style={{width:"200px"}}/>
+          <img
+            className="mb-3"
+            src="media/images/sensibullLogo.svg"
+            style={{ width: "200px" }}
+            alt="sensibull"
+          />
           <p className="text-small text-muted">Options trading platform</p>
         </div>
         <div className="col-4 p-3 mt-5">
-          <img className="mb-3" src="media/images/zerodhaFundhouse.png" style={{width:"250px"}} />
+          <img
+            className="mb-3"
+            src="media/images/zerodhaFundhouse.png"
+            style={{ width: "250px" }}
+            alt="fundhouse"
+          />
           <p className="text-small text-muted">Asset management</p>
         </div>
         <div className="col-4 p-3 mt-5">
-          <img className="mb-3" src="media/images/goldenpiLogo.png" style={{width:"230px"}}/>
+          <img
+            className="mb-3"
+            src="media/images/goldenpiLogo.png"
+            style={{ width: "230px" }}
+            alt="goldenpi"
+          />
           <p className="text-small text-muted">Bonds trading platform</p>
         </div>
         <div className="col-4 p-3 mt-5">
-          <img className="mb-3" src="media/images/dittoLogo.png" style={{width:"180px"}} />
+          <img
+            className="mb-3"
+            src="media/images/dittoLogo.png"
+            style={{ width: "180px" }}
+            alt="ditto"
+          />
           <p className="text-small text-muted">Insurance</p>
         </div>
-        <button
+        <Link
+          to="/signup"
           className="p-2 btn btn-primary fs-5 mb-5 mt-5"
           style={{ width: "20%", margin: "0 auto" }}
         >
           Signup Now
-        </button>
+        </Link>
       </div>
     </div>
   );
